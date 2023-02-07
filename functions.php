307b3c2b951603e2ab2beef85ca576980ccda128
Luis Gomez <luiskaco@gmail.com>
2023-02-07 08:58:13 -0500

    Creando galeria

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ add_action('init', 'gymfitness_menus');
 function gymfitness_scripts_styles()  {
 
     wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
-    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.11');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.12');
 
 }
 
---
+++ page-galeria.php
@@ -0,0 +1,58 @@
+`<?php 
+/*
+   Template Name: Galeria
+*/
+ get_header(); 
+ 
+ // Plantilla personalizada
+ ?>
+
+
+<main class="contenedor seccion"> 
+    <?php
+        while(have_posts()) : the_post();
+
+        the_title('<h1 class="text-center text-primary">','</h1>'); 
+
+
+        // the_content();
+
+        // Obtener la galeria
+        $galeria = get_post_gallery(get_the_ID(), false);  //obtener ela galeria del post actual,  
+                                                        // Pasamos false porque queremos obtener sin html  
+        // Obtener los IDs en un array
+        $galeria_imagenes_id = explode("," , $galeria['ids']);
+
+    
+        // echo "<pre>"; 
+        //     // var_dump($galeria['ids']);
+        //      var_dump( $galeria_imagenes_id);
+        // echo "</pre>";
+
+        ?>
+            <ul class="galeria-imagenes">
+                <?php 
+                    foreach($galeria_imagenes_id as $id){
+                            $galeriaImagenssGrande = wp_get_attachment_image_src($id, 'large')[0];
+                            $galeriaImagenssFull = wp_get_attachment_image_src($id, 'full')[0];
+
+                            ?>
+
+                                <li>
+                                    <a href="<?php echo $galeriaImagenssGrande ;?>"> <img src="<?php  echo $galeriaImagenssFull; ?>" alt="image-galeria-<?php echo $id;?>"></a>
+                                </li>
+
+
+                            <?php 
+                    }
+                ?>
+            </ul>
+
+        <?php
+
+
+        endwhile;?>
+</main>
+
+
+    <?php get_footer();?>
